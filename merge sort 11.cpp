dd8701542cf653e487aa703ad2492f9a128e05bb
domiz <zmallewale@gmail.com>
2024-08-08 09:50:44 +0530

    Update merge sort 11.cpp

--- merge sort 11.cpp
+++ merge sort 11.cpp
@@ -4,7 +4,6 @@
 static int count = 0;
 void merge(int b[], int c[], int a[], int p, int q) {
     int i = 0, j = 0, k = 0;
-
     while (i < p && j < q) {
         count++;
         if (b[i] <= c[j])
@@ -19,18 +18,15 @@ void merge(int b[], int c[], int a[], int p, int q) {
 }
 void mergesort(int a[], float n) {
     int* b, * c, i,j=0, p, q;
-
     if (n > 1) {
         p = floor(n / 2);
         q = ceil(n / 2);
         b = (int*)malloc(p * sizeof(int));
         c = (int*)malloc(q * sizeof(int));
-
         for (i = 0;i < p;i++)
             b[i] = a[i];
         for (i = p;i < n;i++)
             c[j++] = a[i];
-
         mergesort(b, p);
         mergesort(c, q);
         merge(b, c, a, p, q);
